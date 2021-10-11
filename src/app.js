bd9dbd610b26b1ac47eafbf607455e75ae586f89
51m0n397 <simbartolini@gmail.com>
2021-10-11 13:13:39 +0200

    added grid layout

--- src/app.js
+++ src/app.js
@@ -5,6 +5,34 @@ import controller from './controller'
 const app = function () {
     window.app = controller
     loadData().then(() => {
+        const grid = d3.select('#root')
+        .append('div')
+        .attr('id', 'grid')
+        
+        const topicsTableContainer = d3.select('#grid')
+            .append('div')
+            .attr('id', 'topics_table')
+            .attr('class', 'cell')
+
+        const keywordsPlotContainer = d3.select('#grid')
+            .append('div')
+            .attr('id', 'keywords_plot')
+            .attr('class', 'cell')
+
+        const trendsChartContainer = d3.select('#grid')
+            .append('div')
+            .attr('id', 'trends_chart')
+            .attr('class', 'cell')
+
+        const filtersContainer = d3.select('#grid')
+            .append('div')
+            .attr('id', 'filters')
+            .attr('class', 'cell')
+
+        const papersTableContainer = d3.select('#grid')
+            .append('div')
+            .attr('id', 'papers_table')
+            .attr('class', 'cell')
     })
 }
 
