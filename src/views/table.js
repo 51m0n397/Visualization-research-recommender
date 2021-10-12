7e5396aa451434bc94f3a9a35bee6d7df47b780e
51m0n397 <simbartolini@gmail.com>
2021-10-12 13:07:32 +0200

    added sorting to tables

--- src/views/table.js
+++ src/views/table.js
@@ -2,6 +2,12 @@ import * as d3 from 'd3'
 
 export default function () {
     let data = { data: [], selected: null }
+    let sorting = null
+
+    const sortingOrders = [
+        { class: 'aes', function: d3.ascending },
+        { class: 'des', function: d3.descending }
+    ]
 
     let updateData
     let onClick
@@ -11,6 +17,8 @@ export default function () {
             const dom = d3.select(this)
 
             const titles = data.data.length > 0 ? Object.keys(data.data[0]) : []
+            if (titles.length > 0 && sorting == null)
+                sorting = [titles[0], 0]
 
             const table = dom.append('table')
                 .attr('width', '100%')
@@ -19,12 +27,15 @@ export default function () {
 
             const headers = header
                 .selectAll('th')
-                .data(titles).enter()
+                .data(titles)
+                .enter()
                 .append('th')
+                .attr('class', (d) => { if (sorting[0] == d) return sortingOrders[sorting[1]].class })
                 .text((d) => d)
 
             const rows = body.selectAll('tr')
-                .data(data.data).enter()
+                .data(data.data.sort((a, b) => sortingOrders[sorting[1]].function(a[sorting[0]], b[sorting[0]])))
+                .enter()
                 .append('tr')
                 .attr('class', (d) => {
                     if (data.selected && d[data.selected.key] == data.selected.value)
@@ -40,8 +51,21 @@ export default function () {
 
             rows.on('click', (_e, d) => onClick(d))
 
+            headers.on('click', function (event, d) {
+                headers.attr('class', 'header')
+
+                if (d == sorting[0])
+                    sorting[1] = sorting[1] == 1 ? 0 : 1
+                else
+                    sorting[0] = d
+
+                this.className = sortingOrders[sorting[1]].class
+                rows.sort((a, b) => sortingOrders[sorting[1]].function(a[sorting[0]], b[sorting[0]]))
+            })
+
             updateData = function () {
                 const titles = data.data.length > 0 ? Object.keys(data.data[0]) : []
+                sorting = [titles[0], 0]
 
                 const headers = header
                     .selectAll('th')
@@ -49,15 +73,17 @@ export default function () {
                     .join(
                         enter => enter
                             .append('th')
+                            .attr('class', (d) => { if (sorting[0] == d) return sortingOrders[sorting[1]].class })
                             .text((d) => d),
                         update => update
+                            .attr('class', (d) => { if (sorting[0] == d) return sortingOrders[sorting[1]].class })
                             .text((d) => d),
                         exit => exit.remove()
                     )
 
                 const rows = body
                     .selectAll('tr')
-                    .data(data.data)
+                    .data(data.data.sort((a, b) => sortingOrders[sorting[1]].function(a[sorting[0]], b[sorting[0]])))
                     .join(
                         enter => enter
                             .append('tr')
@@ -88,6 +114,18 @@ export default function () {
                             .text((d) => d.value),
                         exit => exit.remove()
                     )
+
+                headers.on('click', function (event, d) {
+                    headers.attr('class', 'header')
+
+                    if (d == sorting[0])
+                        sorting[1] = sorting[1] == 1 ? 0 : 1
+                    else
+                        sorting[0] = d
+
+                    this.className = sortingOrders[sorting[1]].class
+                    rows.sort((a, b) => sortingOrders[sorting[1]].function(a[sorting[0]], b[sorting[0]]))
+                })
             }
         })
     }
